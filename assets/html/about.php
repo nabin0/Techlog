19e221fa5c8ae2e006140ddec0c838282894095a
Nabin <nabinbhatt62@gmail.com>
2021-06-06 20:28:13 +0530

    about us added

--- assets/html/about.php
+++ assets/html/about.php
@@ -1,12 +1,42 @@
 <?php $title="About Us - Techlog";  include '../../templates/top.php'; ?>
 <style>
     <?php include '../../templates/style.css' ?>
+    <?php include '../css/about.css' ?>
 </style>
 
 <!-- Other Content Goes Here -->
+<div class="about-us-cnt">
+    <h2>About Us</h2>
+    <div class="about-txt">
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime aperiam culpa vitae! Aperiam, ex distinctio iste molestiae inventore minima! Dolorem adipisci laborum, totam eius sint explicabo eum unde blanditiis. Eligendi placeat totam alias fugiat assumenda! Nihil, iste consectetur vero possimus molestiae est esse repellat ducimus quam dolores nostrum. Quod, voluptatum!</p>
+    </div>
+</div>
 
-<h1>About TECHLOG</h1>
-
+<div class="meet-team">
+<h2>Meet Team</h2>
+<div class="flex-div">
+<div class="card">
+    <img src="../image/test.jpg" alt="">
+    <div class="prson-desc front">
+        <h3>Name</h3>
+        <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, quis.</h4>
+    </div> 
+     <div class="back">
+        <h3>back content</h3>
+    </div>
+</div>
+<div class="card">
+    <img src="../image/test.jpg" alt="">
+    <div class="prson-desc front">
+        <h3>Name</h3>
+        <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, quis.</h4>
+    </div>
+    <div class="back">
+        <h3>back content</h3>
+    </div>
+</div>
+</div>
+</div>
 
 <script>
     <?php include '../../templates/app.js' ?>
