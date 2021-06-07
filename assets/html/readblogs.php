14fef8acf2f84c22503e559668cdacb5ca28218f
Nabin <nabinbhatt62@gmail.com>
2021-06-07 17:21:57 +0530

    Updated

---
+++ assets/html/readblogs.php
@@ -0,0 +1,13 @@
+<?php $title="Contact Us - Techlog";  $readOn=' class="on" ';  include '../../templates/top.php'; ?>
+<style>
+    <?php include '../../templates/style.css' ?>
+    <?php include '../css/readblogs.css' ?>
+</style>
+
+
+<!-- Other Content goes here -->
+
+<script>
+    <?php include '../../templates/app.js' ?>
+</script>
+<?php include '../../templates/bottom.php'; ?>
---
+++ assets/html/signin.php
@@ -0,0 +1,13 @@
+<?php $title="Contact Us - Techlog";  $signinOn=' class="on" ';  include '../../templates/top.php'; ?>
+<style>
+    <?php include '../../templates/style.css' ?>
+    <?php include '../css/signin.css' ?>
+</style>
+
+<!-- Other Content goes here -->
+
+
+<script>
+    <?php include '../../templates/app.js' ?>
+</script>
+<?php include '../../templates/bottom.php'; ?>
