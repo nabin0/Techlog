7b40d295f99f368745cc41958796e9c0df0f4f59
Shehzad <shehzad87450@gmail.com>
2021-06-11 01:36:02 +0530

    font added in login page

--- assets/html/login.php
+++ assets/html/login.php
@@ -13,6 +13,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/log.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Gothic+A1:wght@200&display=swap" rel="stylesheet">
     <title>Log in page</title>
 </head>
 <body>
